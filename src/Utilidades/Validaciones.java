4df91365d34686c2ca50313f64f974d95d5c325d
José Miguel Verde <jmiguelverdea@icloud.com>
2023-06-01 19:28:24 -0600

    Validaciones Listas

--- src/Utilidades/Validaciones.java
+++ src/Utilidades/Validaciones.java
@@ -2,4 +2,36 @@ package Utilidades;
 
 public class Validaciones {
 
+	public boolean validarEntero(String valor) {
+		boolean respuesta;
+		try {
+			Integer.parseInt(valor);
+			respuesta = true;
+		} catch (Exception ex) {
+			respuesta = false;
+		}
+		return respuesta;
+	}
+	
+	public boolean validarFlotante(String valor) {
+		boolean respuesta;
+		try {
+			Float.parseFloat(valor);
+			respuesta = true;
+		} catch (Exception ex) {
+			respuesta = false;
+		}
+		return respuesta;
+	}
+	
+	public boolean validarCurp(String valor) {
+		boolean respuesta;
+		try {
+			
+			respuesta=true;
+		} catch(Exception ex) {
+			respuesta=false;
+		}
+		return respuesta;
+	}
 }
