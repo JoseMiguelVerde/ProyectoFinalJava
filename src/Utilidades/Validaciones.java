b564f736cf0b38f1a150f3f1a7b60596e1265a5c
José Miguel Verde <jmiguelverdea@icloud.com>
2023-06-01 19:39:01 -0600

    Validaciones Listas 2.0

--- src/Utilidades/Validaciones.java
+++ src/Utilidades/Validaciones.java
@@ -1,5 +1,7 @@
 package Utilidades;
 
+import Entidades.Empleado;
+
 public class Validaciones {
 
 	public boolean validarEntero(String valor) {
@@ -23,15 +25,36 @@ public class Validaciones {
 		}
 		return respuesta;
 	}
+
+	public boolean validarCurp(String curp) {
+
+		String formato="[A-Z]{4}\\d{6}[HM]{1}(AS|BC|BS|CC|CH|CL|CM|CS|DF|DG|GR|GT|HG|JC|MC|MN|MS|NE|NL|NT|OC|PL|QR|QT|SL|SP|SR|TC|TL|TS|VZ|YN|ZS){1}[A-Z]{3}[\\dA-ZÑ]{2}\\d{1}";
+
+		return curp.matches(formato);
+	}
+
+	public boolean validarRfc(String rfc) {
+
+		String formato="[A-Z]{4}\\d{6}[A-Z\\d]{3}";
+
+		return rfc.matches(formato);
+	}
 	
-	public boolean validarCurp(String valor) {
-		boolean respuesta;
-		try {
+	public boolean arregloVacío(Empleado[] arreglo) {
+		boolean respuesta=true;
+		
+		for (int i=0;i<arreglo.length;i++) {
 			
-			respuesta=true;
-		} catch(Exception ex) {
-			respuesta=false;
+			if(arreglo[i]!=null) 
+			{
+				respuesta=false;
+				break;
+			}
+				
 		}
+		
 		return respuesta;
 	}
+
+
 }
