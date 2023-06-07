063968bd6e3f812eacf26fddb90017440cfb4106
unknown <emiseb123@outlook.co>
2023-06-07 09:03:52 -0600

    Clase Nomina

---
+++ src/Entidades/Nomina.java
@@ -0,0 +1,33 @@
+package Entidades;
+
+import java.time.LocalDateTime;
+import Datos.ManipulacionArreglo;
+import Utilidades.Constantes.Deduccion;
+
+public class Nomina {
+	private int idEmpleado;
+	private int idNomina;
+	private LocalDateTime fecha;
+	public Nomina(int idEmpleado)
+	{
+		this.idNomina=manipulacion.dameIdNomina();
+		this.idEmpleado=idEmpleado;
+		this.fecha=LocalDateTime.now();
+	}
+	
+	public int getIdEmpleado() {
+		return idEmpleado;
+	}
+	public void setIdEmpleado(int idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+	public int getIdNomina() {
+		return idNomina;
+	}
+	public void setIdNomina(int idNomina) {
+		this.idNomina = idNomina;
+	}
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+}
