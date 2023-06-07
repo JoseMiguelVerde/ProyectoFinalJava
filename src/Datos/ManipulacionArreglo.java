2b4c5a454394fc8449cc8134424dddcbdf6b24dc
unknown <emiseb123@outlook.co>
2023-06-07 09:28:50 -0600

    Agrega Empleado 2.0

--- src/Datos/ManipulacionArreglo.java
+++ src/Datos/ManipulacionArreglo.java
@@ -1,11 +1,13 @@
 package Datos;
 
 import Entidades.Empleado;
+import Entidades.Gerente;
 
 public class ManipulacionArreglo {
-	private String[][] listaEmpleados=new String[8][7];
+	private String[][] listaEmpleados=new String[10][10];
 	private String[][] listaPercepciones=new String[8][6];
 	private String[][] listaDeducciones=new String[8][6];
+	private String[][] arregloNomina=new String[10][7];
 	
 	private int buscaEspacio() {
 		int indice=-1;
@@ -18,6 +20,7 @@ public class ManipulacionArreglo {
 		}
 		return indice;
 	}
+	
 	public int dameIdEmpleados() {
 		int indice=0, id=0;
 		for(int i=0;i<listaEmpleados.length;i++) {
@@ -32,6 +35,7 @@ public class ManipulacionArreglo {
 		id=indice+1;
 		return id;
 	}
+	
 	public int dameIdPercepcion() {
 		int indice=0, id=0;
 		for(int i=0;i<listaPercepciones.length;i++) {
@@ -46,6 +50,7 @@ public class ManipulacionArreglo {
 		id=indice+1;
 		return id;
 	}
+	
 	public int dameIdDeducciones() {
 		int indice=0, id=0;
 		for(int i=0;i<listaDeducciones.length;i++) {
@@ -60,12 +65,31 @@ public class ManipulacionArreglo {
 		id=indice+1;
 		return id;
 	}
+	
+	public int dameIdNomina() {
+		int indice=0, id=0;
+		for(int i=0;i<arregloNomina.length;i++) {
+			if(arregloNomina[i][0]!=null)
+			{
+				if(Integer.parseInt(arregloNomina[i][0])>indice)
+				{
+					
+					indice=Integer.parseInt(arregloNomina[i][0]);
+					
+				}			
+			}
+		}
+		id=indice+1;
+		return id;
+	}
+	
 	public boolean guardaEmpleado(Empleado datos) {
 		boolean respuesta=false;
 		int indice=buscaEspacio();
 		if(indice!=-1)
 		{
 			/*
+			 
 		 	0-Id
 		 	1-Puesto
 		 	2-Nombre
@@ -75,20 +99,24 @@ public class ManipulacionArreglo {
 		 	6-Fecha de contratacion
 		 	7-Incentivo
 		 	8-Con vehiculo
+		 	
 			*/
 			
 			listaEmpleados[indice][0]=String.valueOf(datos.getId());
-			listaEmpleados[indice][1]=(datos instanceof Gerente?"1":"0");
-			listaEmpleados[indice][2]=datos.getNombre();
-			listaEmpleados[indice][3]=String.valueOf(datos.getGenero().getIdGenero());
+			listaEmpleados[indice][1]=datos.getNombreCompleto();
+			listaEmpleados[indice][2]=(datos instanceof Gerente?"1":"0");
+			listaEmpleados[indice][3]=datos.getRfc();
 			listaEmpleados[indice][4]=datos.getCurp();
-			listaEmpleados[indice][5]=String.valueOf(datos.getSueldoDia());
-			listaEmpleados[indice][6]=String.valueOf(datos.getFechaContratacion());
-			listaEmpleados[indice][7]=(datos instanceof Gerente? String.valueOf(((Gerente)datos).getIncentivo()):"0.0");
-			listaEmpleados[indice][8]=(datos instanceof Gerente? (((Gerente)datos).getConVehiculo()?"1":"0"):"0");
+			listaEmpleados[indice][5]=datos.getDireccion();
+			listaEmpleados[indice][6]=String.valueOf(datos.getGenero().getIdGenero());
+			listaEmpleados[indice][7]=String.valueOf(datos.getEstatus().getEstatus());
+			listaEmpleados[indice][8]=String.valueOf(datos.getFechaNacimiento());
+			listaEmpleados[indice][9]=String.valueOf(datos.getFechaContratacion());
 			respuesta=true;
 			
 		}
 		return respuesta;
 	}
+	
+	
 }
